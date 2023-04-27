17796479db8eb3c2af9dee09cce5e8abfe32cbab
deadsoulartist <deadsoulartist@gmail.com>
2023-04-27 14:10:51 +0530

    feat: added morgan package, setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
 import notesRoute from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
+
+//HTTP request logger middleware for node.js
+//print this in console : GET /api/notes/644918460cf62902a184ec8 500 40.650 ms - 122
+app.use(morgan("dev"));
 
 //to set data format
 app.use(express.json());
