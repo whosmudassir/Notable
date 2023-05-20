90c24537b53dedee5de8a47bcbcf6dbccacf3353
deadsoulartist <deadsoulartist@gmail.com>
2023-05-20 18:37:41 +0530

    feat: added addnote api

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -15,3 +15,19 @@ export const fetchNotes = async () => {
   });
   return resp.json();
 };
+
+interface NoteInput {
+  title: string;
+  text?: string;
+}
+
+export const createNote = async (note: NoteInput) => {
+  const resp = await fetchData("/api/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+  return resp.json();
+};
