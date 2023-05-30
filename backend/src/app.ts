5954915bd838687baa5899f3809a9f3331b10ed7
deadsoulartist <deadsoulartist@gmail.com>
2023-05-30 17:32:35 +0530

    feat: setting up session express launch

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,9 @@ import notesRoute from "./routes/notes";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
 import usersRoute from "./routes/users";
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
@@ -13,6 +16,22 @@ app.use(morgan("dev"));
 
 //to set data format
 app.use(express.json());
+
+//set up express session, cookie setup, store in mongo
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
+  })
+);
 
 //first endpoint
 app.use("/api/users", usersRoute);
